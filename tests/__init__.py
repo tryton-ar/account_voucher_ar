3753a01686cb1b84b71024e990fbc86d895cd186
Sebastián Marró <smarro@thymbra.com>
2012-07-28 02:11:51 -0300

    Add tests

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+#This file is part of the account_voucher_ar module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
+
+from test_account_voucher_ar import suite
