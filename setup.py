711580167cdcccd7c90ba684a0b4ded2791dd217
Sebastián Marró <smarro@thymbra.com>
2013-02-24 21:10:20 -0300

    Fixes issue #4 : Instalaci?n no usa ConfigParser en setup.py y no copia tryton.cfg en el egg
    https://bitbucket.org/thymbra/account_voucher_ar/issue/4/instalaci-n-no-usa-configparser-en-setuppy

--- setup.py
+++ setup.py
@@ -5,15 +5,26 @@
 
 from setuptools import setup
 import re
+import os
+import ConfigParser
 
-info = eval(open('__tryton__.py').read())
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+config = ConfigParser.ConfigParser()
+config.readfp(open('tryton.cfg'))
+info = dict(config.items('tryton'))
+for key in ('depends', 'extras_depend', 'xml'):
+    if key in info:
+        info[key] = info[key].strip().splitlines()
 major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
 
 requires = []
 for dep in info.get('depends', []):
-    if not re.match(r'(ir|res|workflow|webdav)(\W|$)', dep):
+    if not re.match(r'(ir|res|webdav)(\W|$)', dep):
         requires.append('trytond_%s >= %s.%s, < %s.%s' %
                 (dep, major_version, minor_version, major_version,
                     minor_version + 1))
@@ -22,18 +33,18 @@ requires.append('trytond >= %s.%s, < %s.%s' %
 
 setup(name='trytond_account_voucher_ar',
     version=info.get('version', '0.0.1'),
-    description=info.get('description', ''),
-    author=info.get('author', ''),
-    author_email=info.get('email', ''),
-    url=info.get('website', ''),
+    description='Tryton module to add payments and receipts for Argentina',
+    long_description=read('README'),
+    author='Thymbra',
+    url='http://www.thymbra.com/',
     package_dir={'trytond.modules.account_voucher_ar': '.'},
     packages=[
         'trytond.modules.account_voucher_ar',
         'trytond.modules.account_voucher_ar.tests',
     ],
     package_data={
-        'trytond.modules.account_voucher_ar': info.get('xml', []) \
-                + info.get('translation', []),
+        'trytond.modules.account_voucher_ar': info.get('xml', [])
+                + ['tryton.cfg', 'locale/*.po'],
     },
     classifiers=[
         'Development Status :: 1 - Alpha',
