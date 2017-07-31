4d1e7913a543eb1b9790fabb135a57ed022588bd
Adrián Bernardi <adrian.bernardi@gmail.com>
2017-07-31 19:00:51 -0300

    Update Tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,6 @@
-#This file is part of the account_voucher_ar module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+try:
+    from trytond.modules.account_voucher_ar.tests.tests import suite
+except ImportError:
+    from .tests import suite
 
-from test_account_voucher_ar import suite
+__all__ = ['suite']
