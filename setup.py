5c07fc50fb8573b6c68ef6557d42994389c8aa6a
Adrián Bernardi <adrian.bernardi@gmail.com>
2023-09-26 20:36:02 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import io
 import os
 import re
 from configparser import ConfigParser
-from setuptools import setup
+from setuptools import find_packages, setup
 
 MODULE = 'account_voucher_ar'
 PREFIX = 'trytonar'
@@ -17,16 +17,17 @@ MODULE2PREFIX = {
 
 
 def read(fname):
-    return io.open(
+    content = io.open(
         os.path.join(os.path.dirname(__file__), fname),
         'r', encoding='utf-8').read()
+    content = re.sub(
+        r'(?m)^\.\. toctree::\r?\n((^$|^\s.*$)\r?\n)*', '', content)
+    return content
 
 
 def get_require_version(name):
-    #if name.startswith('trytonar_'):
-        #return ''
     if name in LINKS:
-        return '%s@%s' % (name, LINKS[name])
+        return ''  # '%s @ %s' % (name, LINKS[name])
     if minor_version % 2:
         require = '%s >= %s.%s.dev0, < %s.%s'
     else:
@@ -46,18 +47,15 @@ version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-series = '%s.%s' % (major_version, minor_version)
-if minor_version % 2:
-    branch = 'master'
-else:
-    branch = series
 
-download_url = 'https://github.com/tryton-ar/account_voucher_ar/tree/%s' % branch
+url = 'https://github.com/tryton-ar/%s' % MODULE
+download_url = 'https://github.com/tryton-ar/%s/tree/%s.%s' % (
+    MODULE, major_version, minor_version)
 
 LINKS = {
     'trytonar_party_ar': ('git+https://github.com/tryton-ar/'
-        'party_ar.git@%s#egg=trytonar_party_ar-%s' %
-        (branch, series)),
+        'party_ar.git@%s.%s#egg=trytonar_party_ar-%s.%s' %
+        (major_version, minor_version, major_version, minor_version)),
     }
 
 requires = []
@@ -78,19 +76,19 @@ setup(name='%s_%s' % (PREFIX, MODULE),
     description='Tryton module to add payments and receipts for Argentina',
     long_description=read('README'),
     author='tryton-ar',
-    url='https://github.com/tryton-ar/account_voucher_ar',
+    url=url,
     download_url=download_url,
     project_urls={
-        "Bug Tracker": 'https://bugs.tryton.org/',
+        "Bug Tracker": 'https://github.com/tryton-ar/%s/issues' % MODULE,
         "Documentation": 'https://docs.tryton.org/',
         "Forum": 'https://www.tryton.org/forum',
-        "Source Code": 'https://github.com/tryton-ar/account_voucher_ar',
+        "Source Code": url,
         },
     package_dir={'trytond.modules.%s' % MODULE: '.'},
-    packages=[
-        'trytond.modules.%s' % MODULE,
-        'trytond.modules.%s.tests' % MODULE,
-        ],
+    packages=(
+        ['trytond.modules.%s' % MODULE]
+        + ['trytond.modules.%s.%s' % (MODULE, p) for p in find_packages()]
+        ),
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', []) + [
             'tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
@@ -109,25 +107,24 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
         'Topic :: Office/Business :: Financial :: Accounting',
         ],
     license='GPL-3',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=requires,
+    extras_require={
+        'test': tests_require,
+        },
     dependency_links=dependency_links,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
     %s = trytond.modules.%s
     """ % (MODULE, MODULE),
-    test_suite='tests',
-    test_loader='trytond.test_loader:Loader',
-    tests_require=tests_require,
     )
